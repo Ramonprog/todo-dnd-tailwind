47d48a3f1f4b4570654911291e6dada4d10b1035
Ramonprog <programador.ramon@gmail.com>
2023-09-28 08:32:18 -0300

    layoute concluido

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import CrossIcon from './components/icons/CrossIcon'
+import MoonIcon from './components/icons/MoonIcon'
+
 function App() {
 
   return (
-    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain min-h-screen bg-gray-200">
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain min-h-screen bg-gray-300">
       <header className="container mx-auto px-4 pt-8">
         <div className="flex justify-between">
           <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
           <button>
-            Lua
+            <MoonIcon color='#000' />
           </button>
         </div>
         <form className="bg-white rounded-md p-3 my-8 flex gap-4 items-center">
@@ -15,35 +18,37 @@ function App() {
         </form>
       </header>
       <main className="container mx-auto px-4">
-        <div className='bg-white rounded-md px-4'>
-          <article className="">
+        <div className='bg-white rounded-md px-4 '>
+          <article className="flex gap-4 py-4 border-b-gray-400 border-b">
             <button className="rounded-full border-2 border-zinc-300 w-5 h-5 inline-block"></button>
-            <p className="text-gray-500">Complete online javascript curse</p>
-            <button>Exis</button>
+            <p className="text-gray-500 grow">Complete online javascript curse</p>
+            <button className='ml-auto'> <CrossIcon /></button>
           </article>
-          <article className="">
-            <button>Circulo</button>
-            <p>Complete online javascript curse</p>
-            <button>Exis</button>
+          <article className="flex gap-4 py-4 border-b-gray-400 border-b">
+            <button className="rounded-full border-2 border-zinc-300 w-5 h-5 inline-block"></button>
+            <p className="text-gray-500 grow">Complete online javascript curse</p>
+            <button className='ml-auto'> <CrossIcon /></button>
+          </article>    <article className="flex gap-4 py-4 border-b-gray-400 border-b">
+            <button className="rounded-full border-2 border-zinc-300 w-5 h-5 inline-block"></button>
+            <p className="text-gray-500 grow">Complete online javascript curse</p>
+            <button className='ml-auto'> <CrossIcon /></button>
           </article>
-          <article className="">
-            <button>Circulo</button>
-            <p>Complete online javascript curse</p>
-            <button>Exis</button>
-          </article>
-          <section>
-            <span>5 items left</span>
-            <button>clear completed</button>
+
+          <section className='py-4 px-4 flex justify-between'>
+            <span className='text-gray-400'>5 items left</span>
+            <button className='text-gray-400'>clear completed</button>
           </section>
         </div>
       </main>
-      <section className="container mx-auto px-4">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+      <section className="container mx-auto px-4 mt-8">
+        <div className='bg-white p-4 rounded-md flex justify-center gap-4'>
+          <button className='hover:text-blue-600' >All</button>
+          <button className='hover:text-blue-600'>Active</button>
+          <button className='hover:text-blue-600'>Completed</button>
+        </div>
       </section>
 
-      <p className="text-center">Drag and drop to reorder list</p>
+      <p className="text-center mt-8">Drag and drop to reorder list</p>
     </div>
   )
 }
