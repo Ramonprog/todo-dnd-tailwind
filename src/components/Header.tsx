e238d0a12d353fdcb1a76d17c51bf47798c56042
Ramonprog <programador.ramon@gmail.com>
2023-10-03 20:58:27 -0300

    mudando modo com o localStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,33 @@
 import MoonIcon from '../components/icons/MoonIcon'
+import SunIcon from '../components/icons/SunIncon'
+import { useState, useEffect } from 'react'
+
 const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme === 'dark';
+    });
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+        localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+    }, [darkMode]);
+
+    const toggleDarkMode = () => {
+        setDarkMode(prevDarkMode => !prevDarkMode);
+    };
+
     return (
         <header className="container mx-auto px-4 pt-8">
             <div className="flex justify-between">
                 <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
-                <button>
-                    <MoonIcon color='#000' />
+                <button onClick={toggleDarkMode}>
+                    {darkMode ? <SunIcon /> : <MoonIcon />}
                 </button>
             </div>
         </header>
